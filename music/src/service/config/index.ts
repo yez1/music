130f93644f1f050dc6aab1edfaf6c085ca86ea4f
叶子 <yezi@yezideMacBook-Air.local>
2026-02-11 16:37:30 +0800

    内网穿网址5

--- music/src/service/config/index.ts
+++ music/src/service/config/index.ts
@@ -1,30 +1,16 @@
-// import axios from 'axios';
-// //本地开发环境
-// // export const BASE_URL = "http://localhost:3000";
-
-
-
-// export const TIME_OUT = 10000;
-
-// // console.log(process.env.NODE_ENV)
-
-// // console.log(process.env.REACT_APP_BASE_URL)
-
-
 import axios from 'axios';
+//本地开发环境
+// export const BASE_URL = "http://localhost:3000";
+// config.ts
+export const BASE_URL = process.env.REACT_APP_API_URL || "https://hey-belle-underlying-wines.trycloudflare.com";
 
-// 1. 定义地址
-export const BASE_URL = "https://slick-seals-float.loca.lt";
+
 export const TIME_OUT = 10000;
 
-// 2. 创建并导出 axios 实例
-const service = axios.create({
-    baseURL: BASE_URL, 
-    timeout: TIME_OUT,
-    headers: {
-        'Bypass-Tunnel-Reminder': 'true' 
-    }
-});
+// console.log(process.env.NODE_ENV)
 
-// 必须导出这个 service，其他页面才能引用它
-export default service;
+// console.log(process.env.REACT_APP_BASE_URL)
+
+
+
+
