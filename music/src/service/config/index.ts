dc0f1368b9adf8389fd418da54abd8cda6d6d504
叶子 <yezi@yezideMacBook-Air.local>
2026-02-11 16:06:58 +0800

    内网穿网址透1

--- music/src/service/config/index.ts
+++ music/src/service/config/index.ts
@@ -1,6 +1,16 @@
+import axios from 'axios';
 //本地开发环境
 // export const BASE_URL = "http://localhost:3000";
-export const BASE_URL = "https://giant-zoos-itch.loca.lt";
+// export const BASE_URL = "https://giant-zoos-itch.loca.lt";
+// 在你创建 axios 实例或配置拦截器的地方添加 headers
+    export const service = axios.create({
+        baseURL: 'https://giant-zoos-itch.loca.lt', // 确保这是你最新的穿透地址
+        timeout: 5000,
+        headers: {
+        // 关键：这行代码会告诉 LocalTunnel 跳过验证页
+        'Bypass-Tunnel-Reminder': 'true' 
+        }
+    });
 
 export const TIME_OUT = 10000;
 
