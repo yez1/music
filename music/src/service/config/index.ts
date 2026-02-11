228504d4d1cce9368de59b94720616fe2331bdb9
叶子 <yezi@yezideMacBook-Air.local>
2026-02-11 16:09:11 +0800

    内网穿网址透2

--- music/src/service/config/index.ts
+++ music/src/service/config/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 // export const BASE_URL = "http://localhost:3000";
 // export const BASE_URL = "https://giant-zoos-itch.loca.lt";
 // 在你创建 axios 实例或配置拦截器的地方添加 headers
-    export const service = axios.create({
+     const service = axios.create({
         baseURL: 'https://giant-zoos-itch.loca.lt', // 确保这是你最新的穿透地址
         timeout: 5000,
         headers: {
