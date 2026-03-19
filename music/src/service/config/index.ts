da69636fad858adf82852d27f6c8cb9f0150f93d
叶子 <yezi@yezideMacBook-Air.local>
2026-03-19 13:26:31 +0800

    319-1

--- music/src/service/config/index.ts
+++ music/src/service/config/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const BASE_URL = process.env.REACT_APP_API_URL || "https://eight-bushes-exist.loca.lt";
+export const BASE_URL = process.env.REACT_APP_API_URL || "https://alternative-presentations-consecutive-lips.trycloudflare.com";
 
 
 export const TIME_OUT = 10000;
